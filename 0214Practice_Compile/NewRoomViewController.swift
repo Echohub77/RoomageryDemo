7fc95690350057554b5116dd9fbede0ee4b38078
EchoHub77 <doublemenglish@gmail.com>
2017-02-14 16:24:56 +0900

    Before Realm: touch error(moveThis not available)
    furn add, room add, touch error

--- 0214Practice_Compile/NewRoomViewController.swift
+++ 0214Practice_Compile/NewRoomViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 let DidUserDoneInput = Notification.Name(rawValue: "DidInput")
 
+
+
+
+
+
+
 class insertRoomSize: UIViewController {
     
     @IBOutlet weak var roomWidth: UITextField!
     @IBOutlet weak var roomHeight: UITextField!
     @IBAction func roomSizeConfirmed(_ sender: UIButton) {
-        NotificationCenter.default.post(name: DidUserDoneInput, object: nil, userInfo: ["widthR":CGFloat(Float(self.roomWidth.text!)!),"heightR":CGFloat(Float(self.roomHeight.text!)!)])
+//질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-질문-
+//        if roomWidth.text?.characters.count < 1 ||
+//          roomHeight.text?.characters.count < 1 {
+//            let typeRoomInfo: UIAlertController = UIAlertController(title: "Type Your Room Information", message: "Your room information is missing. Please fill up the blanks", preferredStyle: UIAlertControllerStyle.alert)
+//            let typeRoomOK: UIAlertAction = UIAlertAction(title: "Got it", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in })
+//            typeRoomInfo.addAction(typeRoomOK)
+//            self.present(typeRoomInfo, animated: true, completion: nil)
+//        } else {
+        NotificationCenter.default.post(name: DidUserDoneInput, object: nil, userInfo: ["widthR":CGFloat(Float(self.roomWidth.text!)!/12),"heightR":CGFloat(Float(self.roomHeight.text!)!/12)])
+//        }
     }
     
-    
-    
-
-    
 }
+
+
+
+
+
+
+
 
 
 class NewRoomViewController: UIViewController {
@@ -49,12 +67,37 @@ class NewRoomViewController: UIViewController {
     }
     
     
-    //---------------------------- Furniture ------------------------------//
+    //---------------------------- Furniture -- Add -----------------------//
     
     var lastFrame: CGRect!
     
     override func viewWillAppear(_ animated: Bool) {
     }
+    
+    func didReceiveUserFurnitureInputNotification(noti: Notification) {
+        if let info = noti.userInfo, let widthF = info["widthF"] as? CGFloat, let depthF = info["depthF"] as? CGFloat {
+            self.addNewFurniture(width: widthF, height: depthF)
+        }
+    }
+    
+    func addNewFurniture(width: CGFloat, height: CGFloat) {
+        let widthF: CGFloat = width
+        let depthF: CGFloat = height
+        let furnitureAdded: UIView = UIView(frame: CGRect(x: self.base.center.x - (width / 2.0), y: self.base.center.y - (height / 2.0), width: widthF
+            , height: depthF))
+        furnitureAdded.backgroundColor = UIColor.brown
+        self.base.bringSubview(toFront: furnitureAdded)
+        view.addSubview(furnitureAdded)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveUserRoomInputNotification(noti:)), name: DidUserDoneInput, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveUserFurnitureInputNotification(noti:)), name: DidUserDoneInput, object: nil)
+        
+    }
+    
+    //---------------------------- Furniture -- Move -----------------------//
     
     func viewShouldMove(subview: UIView, movingTo location: CGPoint) -> Bool {
         
@@ -87,78 +130,51 @@ class NewRoomViewController: UIViewController {
         }
         return true
     }
-    
-    func didReceiveUserFurnitureInputNotification(noti: Notification) {
-        if let info = noti.userInfo, let widthF = info["widthF"] as? CGFloat, let depthF = info["depthF"] as? CGFloat {
-            self.addNewFurniture(width: widthF, height: depthF)
-        }
-    }
-    
-    func addNewFurniture(width: CGFloat, height: CGFloat) {
-        let widthF: CGFloat = width
-        let depthF: CGFloat = height
-        let furnitureAdded: UIView = UIView(frame: CGRect(x: self.base.center.x - (width / 2.0), y: self.base.center.y - (height / 2.0), width: widthF
-            , height: depthF))
-        furnitureAdded.backgroundColor = UIColor.brown
-        self.base.bringSubview(toFront: furnitureAdded)
-        view.addSubview(furnitureAdded)
-    }
-    
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-         NotificationCenter.default.addObserver(self, selector: #selector(didReceiveUserRoomInputNotification(noti:)), name: DidUserDoneInput, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveUserFurnitureInputNotification(noti:)), name: DidUserDoneInput, object: nil)
-        
-    }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first?.location(in: self.base) else {
-            print("터치 인식 불가")
-            return
-        }
-        
-        for subview in self.base.subviews {
-            if subview.frame.contains(touch) == true {
-//                self.base.bringSubview(toFront: UIView)
-                break
-            }
-        }
-        
-//        if touch == nil {
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first?.location(in: self.base) else {
+//            print("터치 인식 불가")
 //            return
 //        }
-        
-//        let location = touches.first?.location(in: self.base)
-//        if let position = location, let currentMoving = touch {
-//            if viewShouldMove(subview: currentMoving, movingTo: position) {
-//                self.t
+//        
+//        for subview in self.base.subviews {
+//            if subview.frame.contains(touch) == true {
+////                self.base.bringSubview(toFront: UIView)
+//                break
 //            }
 //        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        
+//        if base.subviews == nil {
+//            return
+//        }
+//        
 //        let location = touches.first?.location(in: self.base)
-        
-//        if let position = location, let putHere = self.
-    }
+//        if let position = location, let currentMoving = self.base.subviews {
+//            if viewShouldMove(subview: currentMoving, movingTo: position) {
+//                self.base.subviews.center = position
+//            }
+//        }
+//    }
+//    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let location = touches.first?.location(in: self.base)
+//        
+//        if let position = location, let putHere = self.base.subviews {
+//            if viewShouldMove(subview: putHere, movingTo: position) {
+//                self.base.subviews.center = position
+//            }
+//        }
+//    }
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.moveThis = nil
+//        
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func saveRoom(_ sender: Any) {
         self.dismiss(animated: true) {
@@ -166,6 +182,16 @@ class NewRoomViewController: UIViewController {
     }
     
 }
+
+
+
+
+
+
+
+
+
+
 
 class NewFurniture: UIViewController {
     
@@ -183,7 +209,7 @@ class NewFurniture: UIViewController {
         
     }
     @IBAction func addFurnitureToTheRoom(_ sender: UIButton) {
-        NotificationCenter.default.post(name: DidUserDoneInput, object: nil, userInfo: ["widthF":CGFloat(Float(self.furnitureWidth.text!)!),"depthF":CGFloat(Float(self.furnitureDepth.text!)!)])
+        NotificationCenter.default.post(name: DidUserDoneInput, object: nil, userInfo: ["widthF":CGFloat(Float(self.furnitureWidth.text!)!/12),"depthF":CGFloat(Float(self.furnitureDepth.text!)!/12)])
         self.dismiss(animated: true) {
         }
     }
@@ -204,6 +230,14 @@ class NewFurniture: UIViewController {
 
 
 
-
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 
